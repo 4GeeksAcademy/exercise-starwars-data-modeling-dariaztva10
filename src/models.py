851879e7dbc4c91198e5d56d758e8340c1d624f5
Daria <149780226+dariaztva10@users.noreply.github.com>
2024-09-16 13:27:14 +0000

    Primera entrega del proyecto

--- src/models.py
+++ src/models.py
@@ -7,26 +7,67 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = "usuario"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable = False, unique=True)
+    password = Column(String(250), nullable = False)
+    suscription = Column(String(250), nullable = False)
+    name = Column(String(250), nullable = False)
+    surname = Column(String(250), nullable = False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    favoritod = relationship('favoritos', back_populates='usuario')
+
+
+class Favoritos(Base):
+    __tablename__ = "favoritos"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+     # Claves foráneas para relacionar con personajes, planetas y vehículos
+    personajes_id = Column(Integer, ForeignKey('personajes.id'), nullable=True)
+    planetas_id = Column(Integer, ForeignKey('planetas.id'), nullable=True)
+    vehiculos_id = Column(Integer, ForeignKey('vehiculos.id'), nullable=True)
+
+    # Relacion con usuario
+    usuario_id = Column(Integer, ForeignKey('usuario.id'), nullable=True)
+    usuario = relationship('Usuario', back_populates='favoritos')
+
+    # Relaciones con las demás tablas
+    personaje = relationship('Personajes')
+    planeta = relationship('Planetas')
+    vehiculo = relationship('Vehiculos')
+    
+
+class Personajes(Base):
+    __tablename__ = "personajes"
+    id =  id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable = False)
+
+
+class Planetas(Base):
+    __tablename__ = "planetas"
+    id =  id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable = False)
+
+
+class Vehiculos(Base):
+    __tablename__ = "vehiculos"
+    id =  id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable = False)
+
+
 
     def to_dict(self):
         return {}
 
+
+#CREANDO LA BASE DE DATOS SQLite
+engine = create_engine('sqlite:///test.db')
+
+
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result = render_er(Base, 'diagram.png')
+    print("¡Éxito! Revisa el archivo diagram.png :)")
+except Exception as e:
+    print("Hubo un problema al generar el diagrama :(")
+    raise e
